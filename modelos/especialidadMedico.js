a5e2404034631c64c42b7d98f9c078471c9f35c6
FacuuLopez <faculopez93@hotmail.com.ar>
2023-09-11 21:10:51 -0300

    saque entidad modelo medico-consulta porque el precio para el medico es unico

--- modelos/especialidadMedico.js
+++
@@ -1,9 +0,0 @@
-import { Model, DataTypes as DT } from "sequelize";
-
-class EspecialidadMedico extends Model {}
-
-EspecialidadMedico.init({
-    tarifa:{},
-})
-
-export default EspecialidadMedico;
