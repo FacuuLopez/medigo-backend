94aaec47bbea060c516d7f0b178a023296292429
sosamatias1 <sosamatias171@gmail.com>
2023-10-12 17:33:55 -0300

    Se corrigen los esquemas con la sintaxis para la funcion checkSchema

--- validadores/esquemas/personas.js
+++ validadores/esquemas/personas.js
@@ -1,31 +1,51 @@
-import { body } from "express-validator";
+export const personaSchema = {
+    nombre: {
+      in: ['body'],
+      isString: {
+        errorMessage: 'El nombre debe ser una cadena.',
+      },
+      notEmpty: {
+        errorMessage: 'El nombre es obligatorio.',
+      },
+      isLength: {
+        options: { min: 2 },
+        errorMessage: 'El nombre debe tener al menos 2 caracteres.',
+      },
+    },
+    apellido: {
+      in: ['body'],
+      isString: {
+        errorMessage: 'El apellido debe ser una cadena.',
+      },
+      notEmpty: {
+        errorMessage: 'El apellido es obligatorio.',
+      },
+      isLength: {
+        options: { min: 2 },
+        errorMessage: 'El apellido debe tener al menos 2 caracteres.',
+      },
+    },
+    sexo: {
+      in: ['body'],
+      isString: {
+        errorMessage: 'El sexo debe ser una cadena.',
+      },
+      notEmpty: {
+        errorMessage: 'El sexo es obligatorio.',
+      },
+      isIn: {
+        options: [['M', 'F', 'O']],
+        errorMessage: "El sexo debe ser 'M', 'F' u 'O'.",
+      },
+    },
+    fechaNacimiento: {
+      in: ['body'],
+      isDate: {
+        errorMessage: 'La fecha de nacimiento debe ser una fecha válida.',
+      },
+      notEmpty: {
+        errorMessage: 'La fecha de nacimiento es obligatoria.',
+      },
+    },
+  };
 
-export const personaSchema = [
-
-    body("nombre")
-    .isString()
-    .notEmpty()
-    .withMessage("El nombre es obligatorio")
-    .isLength({ min: 2 })
-    .withMessage("El nombre debe tener al menos 2 caracteres"),
-  
-    body("apellido")
-    .isString()
-    .notEmpty()
-    .withMessage("El apellido es obligatorio")
-    .isLength({ min: 2 })
-    .withMessage("El apellido debe tener al menos 2 caracteres"),
-    
-    body("sexo")
-    .isString()
-    .notEmpty()
-    .withMessage("El sexo es obligatorio")
-    .isIn(["M", "F", "O"])
-    .withMessage("El sexo debe ser 'M', 'F' u 'O'"),
-    
-    body("fechaNacimiento")
-    .isDate()
-    .notEmpty()
-    .withMessage("La fecha de nacimiento es obligatoria."),
-    
-]
