17b2dfb817a55aacba716ad5ffd5fdb72c6c719a
Uriel <urielszw@gmail.com>
2023-10-31 20:20:54 -0300

    main: solicitarConsultaMedico add user data

--- controladores/consultas.js
+++ controladores/consultas.js
@@ -171,10 +171,19 @@ class consultasController {
         },
       });
 
+      const usuarioDeLaConsulta = await persona.findOne({
+        where: {
+          id: consultaDeMedico.personaId,
+        },
+      });
+
       res.status(200).json({
         success: true,
         message: "Consulta",
-        result: consultaDeMedico,
+        result: {
+          ...consultaDeMedico,
+          ...usuarioDeLaConsulta,
+        },
       });
     } catch (error) {
       res.status(500).send(error.message);
