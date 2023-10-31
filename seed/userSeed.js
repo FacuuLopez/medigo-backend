dcc17f5506699e3bf0c2f0b84192dd14adcab2b9
Mariano <mariano.psico@gmail.com>
2023-10-30 22:15:38 -0300

    ajustes es seed de consultasSeed para cubrir casos de uso

--- seed/userSeed.js
+++ seed/userSeed.js
@@ -1,29 +1,32 @@
-import { crearNuevoCliente } from '../controladores/clientes.js';
-import { crearMedico } from '../controladores/medicos.js';
-import { ENUM_MEDICO_ESPECIALIDADES, ENUM_USUARIO_ESTADOS } from '../utils/enums.js';
+import { crearNuevoCliente } from "../controladores/clientes.js";
+import { crearMedico } from "../controladores/medicos.js";
+import {
+  ENUM_MEDICO_ESPECIALIDADES,
+  ENUM_USUARIO_ESTADOS,
+} from "../utils/enums.js";
 
 const crearClientes = async () => {
   try {
     const numUsers = 20;
     const grupoFamiliar = [
       {
-        nombre: 'hijo',
-        apellido: 'prodigo',
-        sexo: 'femenino',
-        fechaNacimiento: new Date('2022-10-03T12:00:00')
-      }
-    ]
+        nombre: "hijo",
+        apellido: "prodigo",
+        sexo: "femenino",
+        fechaNacimiento: new Date("2022-10-03T12:00:00"),
+      },
+    ];
     for (let i = 1; i <= numUsers; i++) {
       const username = `cliente${i}@example.com`;
       const password = `password${i}`;
       const nombre = `cosme`;
-      const apellido = 'fulanito';
+      const apellido = "fulanito";
       const dni = `223335${i}`;
       const telefono = `123456789${i}`;
       const direccion = `Calle-${i}`;
       const estado = ENUM_USUARIO_ESTADOS.desconectado;
-      const sexo = i % 2 === 0 ? 'F' : 'M';
-      const fechaNacimiento = new Date('1990-10-03T12:00:00');
+      const sexo = i % 2 === 0 ? "F" : "M";
+      const fechaNacimiento = new Date("1990-10-03T12:00:00");
 
       const cliente = {
         username,
@@ -40,29 +43,35 @@ const crearClientes = async () => {
       };
       await crearNuevoCliente(cliente);
     }
-    console.log('Usuarios creados exitosamente');
+    console.log("Usuarios creados exitosamente");
   } catch (error) {
     console.log(error.message);
   }
-}
+};
 
-const crearMedicos = async() => {
+const crearMedicos = async () => {
   try {
     const numUsers = 8;
     for (let i = 1; i <= numUsers; i++) {
       const username = `medico${i}@example.com`;
       const password = `password${i}`;
       const nombre = `cosme`;
-      const apellido = 'fulanito';
+      const apellido = "fulanito";
       const dni = `223335${i}`;
       const telefono = `123456789${i}`;
       const direccion = `Calle-${i}`;
-      const estado = Math.random() > 0.5 ? ENUM_USUARIO_ESTADOS.desconectado : ENUM_USUARIO_ESTADOS.conectado;
-      const sexo = i % 2 === 0 ? 'F' : 'M';
-      const fechaNacimiento = new Date('1990-10-03T12:00:00');
+      const estado =
+        Math.random() > 0.5
+          ? ENUM_USUARIO_ESTADOS.desconectado
+          : ENUM_USUARIO_ESTADOS.conectado;
+      const sexo = i % 2 === 0 ? "F" : "M";
+      const fechaNacimiento = new Date("1990-10-03T12:00:00");
       const precio = 3000;
       const radioAccion = 1.0;
-      const especialidad = i % 2 ? ENUM_MEDICO_ESPECIALIDADES.clinico : ENUM_MEDICO_ESPECIALIDADES.pediatra;
+      const especialidad =
+        i % 2
+          ? ENUM_MEDICO_ESPECIALIDADES.clinico
+          : ENUM_MEDICO_ESPECIALIDADES.pediatra;
       const nroMatricula = `AA123${i}`;
       const medico = {
         username,
@@ -78,23 +87,20 @@ const crearMedicos = async() => {
         precio,
         especialidad,
         nroMatricula,
-        radioAccion
-      }
+        radioAccion,
+      };
       const nuevoMedico = await crearMedico(medico);
     }
-
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 const createUsers = async () => {
-  try{
+  try {
     await crearClientes();
     await crearMedicos();
-  }catch{
-
-  }
+  } catch {}
 };
 
 export default createUsers;
